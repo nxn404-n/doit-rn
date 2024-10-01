e569d0331ab55f1ab61369b5bffc04f395dadc77
nxn404-n <nafeurrahmanadil@gmail.com>
2024-10-01 13:51:10 +0600

    Add the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Homepage from "./components/Homepage";
+
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div className="flex items-center justify-center h-screen">
+      <Homepage />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <div className="bg-[#FAEBD7] w-3/4 h-4/5">
+      <h1></h1>
+    </div>
+  )
+}
+
+export default Homepage;
