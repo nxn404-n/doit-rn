1390ff8ca3c5f4e0acd83718776b3ad78ff6746b
nxn404-n <nafeurrahmanadil@gmail.com>
2024-10-02 18:14:27 +0600

    Add redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className='flex items-center justify-center h-screen'>
-      <App />
-    </div>
+    <Provider store={store}>
+      <div className='flex items-center justify-center h-screen'>
+        <App />
+      </div>
+    </Provider>
   </StrictMode>
 );
